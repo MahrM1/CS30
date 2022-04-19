80a0b2e67fbf5b950f700db90f935011e38a501c
1100015963 <1100015963@L815WD220014.edu.cbe.ab.ca>
2022-04-19 09:51:58 -0600

    up data Chapter11

---
+++ Chapter11/src/MyFile_part1of2.java
@@ -0,0 +1,18 @@
+import java.io.File;
+
+public class MyFile_part1of2 
+{
+	public static void main(String[] args) 
+	{
+		File textFile = new File("C:/Chapter11/test file.txt");
+	
+		if (textFile.exists()) 
+		{
+			System.out.println("File already exists. ");
+		}		
+		else 
+		{
+			System.out.println("File does not exists. ");
+		}
+	}
+}
