63776c7dc6c58f0e2d7d78fc7c53627c4be592f1
1100015963 <1100015963@L815WD220014.edu.cbe.ab.ca>
2022-06-09 10:11:20 -0600

    Up data phidget rovor

--- PhidgetRover/src/Move1.java
+++ PhidgetRover/src/Move1.java
@@ -4,7 +4,7 @@
 //Add Phidgets Library
 import com.phidget22.*;
 
-public class Move {
+public class Move1 {
     public static void main(String[] args) throws Exception {
 
         //Connect to wireless rover
@@ -23,11 +23,11 @@ public class Move {
         rightMotors.open(5000);
 
         //Move forward at full speed
-        leftMotors.setTargetVelocity(1);
-        rightMotors.setTargetVelocity(1);
+        leftMotors.setTargetVelocity(0.5);
+        rightMotors.setTargetVelocity(0.5);
 
         //Wait for 1 second
-        Thread.sleep(1000);
+        Thread.sleep(1000,2000);
 
         //Stop motors
         leftMotors.setTargetVelocity(0);
